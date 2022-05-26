ef83d312abc09f17640840e2072dce442083d6bb
Saeed Sikandar <saeedcs98@gmail.com>
2022-05-26 12:56:21 +0500

    Add percentage work - basics

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Char b = 'f';
+
+
+            ///Console.WriteLine("This is Integer = "+i+ "\n");
+            ///Console.WriteLine("This is Double = " + d + "\n");
+            //Console.WriteLine("This is Integer = " + i + "\n");
+            //Console.WriteLine("This is Boolean = " + b + "\n");
+            // Console.WriteLine("This is String = " + s + "\n");
+
+            //Double percentage = Convert.ToDouble(Console.ReadLine());
+            Double percentage = Convert.ToDouble(Console.ReadLine());
+
+            //for(Int32 )
+
+            if(percentage >= 80 ) Console.Write("A+ G2rade" );
+            
+            else if (percentage >= 70 && percentage < 80) Console.Write("\n A Grade");
+
+            else if (percentage >= 60 && percentage < 70) Console.Write("\n B Grade");
+
+            else if (percentage >= 50 && percentage < 60) Console.Write("\n C Grade");
+                
+            else Console.Write("\n Fail");
+
+            Console.WriteLine("\n Would you like to add more results! Type Y for Yes and N for No");
+
+            b = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("bb"+b);
+                
+
+
+           Console.ReadKey();
+
+        }
+
+    }
+}
